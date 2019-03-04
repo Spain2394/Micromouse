c8988cb3c45fa8abe1be78a3c78816153ed4f058
Allen Spain <aspain2394@gmail.com>
2019-03-04 17:29:20 -0500

    upate

--- framework/core_demo.py
+++ framework/core_demo.py
@@ -4,7 +4,7 @@
 
 from map import Map
 from mouse import Micromouse
-from strategy import StrategyTestMultiDFS
+from strategy import StrategyTestRendezvous
 from controller import COREController
 from socket import *
 
