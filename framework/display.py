b15f3b1ae16ceb741f85b4b19eb89c90875ec2d5
Allen Spain <aspain2394@gmail.com>
2019-03-04 00:14:13 -0500

    upate

--- framework/display.py
+++ framework/display.py
@@ -37,7 +37,7 @@ while True:
               str(otherMap['down']) + ',left:' + str(otherMap['left']) + 'right:' + str(otherMap['right']))
         print(type(otherMap['x']))
         position.append(otherMap['x'])
-		print(position)
+		print('' + str(position[0]))
 
 
 # for n in range(4):
