f61e4464e74ab1a599998954a2ceb5edbac969fa
Allen Spain <aspain2394@gmail.com>
2019-03-04 17:09:11 -0500

    upate

--- framework/core_demo.py
+++ framework/core_demo.py
@@ -17,5 +17,6 @@ initPoint = {'1':(0,0), '2':(15,0), '3':(0,15), '4':(15,15)} # 4 robot initial p
 num_bots=len(initPoint)
 micromouse.setMotorController(COREController(index, initPoint[index], '10.0.0.254'))
 micromouse.setInitPoint(initPoint[index][0], initPoint[index][1])
-micromouse.addTask(StrategyTestMultiDFS(micromouse))
+# micromouse.addTask(StrategyTestMultiDFS(micromouse))
+micromouse.addTask(StrategyTestRendezvous(micromouse))
 micromouse.run()
