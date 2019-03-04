b1401771a5560592e61235b7243ee914ca64d238
Allen Spain <aspain2394@gmail.com>
2019-03-04 00:38:26 -0500

    upate

--- framework/display.py
+++ framework/display.py
@@ -36,5 +36,6 @@ while True:
         print('(' + str(otherMap['x']) + ', ' + str(otherMap['y']) + ')  up:' + str(otherMap['up']) + ',down:' + str(
             otherMap['down']) + ',left:' + str(otherMap['left']) + 'right:' + str(otherMap['right']))
         print(type(otherMap['x']))
-        position.append(1)
+        robot_pose = str('rx' +'(' + str(otherMap['x']) + ',' str(otherMap['y']) + ')')
+        # position.append("r1" + '(' + str(otherMap['x']) + ',' str(otherMap['y'])')')
         print('position', str(position))
