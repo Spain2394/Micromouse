7afdbd2f883bd8729d0c49814a9cfa599ac37f18
Allen Spain <aspain2394@gmail.com>
2019-03-04 21:51:27 -0500

    upate

--- framework/strategy.py
+++ framework/strategy.py
@@ -185,108 +185,108 @@ class StrategyTestMultiDFS(Strategy):
         sleep(0.5)
 
 
-class StrategyTestRendezvous(Strategy):
-    mouse = None
-    # isVisited = []
-    path = []
-    isBack = False
-    network = None
-	neighbors_states = {}
-	topological_neighbors = []
-	num_bots = 0
-	stop_condition = False
-	whoami = -1
-	dx = -1.0
-	dy = -1.0
-
-    def __init__(self, mouse, num_bots, initPoint):
-		# add
-		print("hello")
-		self.mouse = mouse
-		self.num_bots = num_bots
-		for i in range(1, num_bots + 1):
-			if initPoint[str(i)] != (self.mouse.x, self.mouse.y):
-				self.neighbors_states[i] = {'robot:' i, 'x': initPoint[str(i)][0], 'y': initPoint[str(i)][1]}
-			else: self.whoami = i
-        self.network = NetworkInterface()
-        self.network.initSocket()
-        self.network.startReceiveThread()
-
-    def checkFinished(self):
-		threshold = 2.0
-		if self.dx <= threshold and self.dy <= threshold:
-			self.stop_condition = True
-		else:
-			self.stop_condition = False
-        return self.stop_condition
-
-    def go(self):
-        self.mouse.senseWalls()
-        print(self.mouse.getCurrentCell().getWhichIsWall())
-        sendData = {'robot': self.whoami , 'x': self.mouse.x, 'y': self.mouse.y, 'up': not self.mouse.canGoUp(
-        ), 'down': not self.mouse.canGoDown(), 'left': not self.mouse.canGoLeft(), 'right': not self.mouse.canGoRight()}
-        self.network.sendStringData(sendData)
-        recvData = self.network.retrieveData()
-
-		# data recieved from neighbors
-		while recvData:
-            some_data = recvData
-            if some_data['up']:
-                self.mouse.mazeMap.setCellUpAsWall(cell)
-            if some_data['down']:
-                self.mouse.mazeMap.setCellDownAsWall(cell)
-            if some_data['left']:
-                self.mouse.mazeMap.setCellLeftAsWall(cell)
-            if some_data['right']:
-                self.mouse.mazeMap.setCellRightAsWall(cell)
-            recvData = self.network.retrieveData()
-
-		# calculate the direction to pursue
-		def slope(self, num_bots):
-			# find largest delta x, and delta deltay
-			for k in range(1, num_bots + 1):
-				if k != self.whoami:
-					dx_temp = self.mouse.x - self.neighbors_states[str(k)]['x']
-					dy_temp = self.mouse.y - self.neighbors_states[str(k)]['y']
-
-					if dx_temp > dx:
-						self.dx = dx_temp # right/left
-					if dy_temp > dy:
-						self.dy = dy_temp # up/down
-
-
-        if self.mouse.canGoLeft() and self.dx < 0 and (self.dx > self.dy):
-            self.path.append([self.mouse.x, self.mouse.y])
-			self.mouse.goLeft()
-			self.neighbors_states[self.whoami] = {'robot:' whoami, 'x': self.mouse.x, 'y': self.mouse.y}
-        elif self.mouse.canGoUp() and self.dy > 0 and (self.dy > self.dx):
-            self.path.append([self.mouse.x, self.mouse.y])
-            self.mouse.goUp()
-			self.neighbors_states[self.whoami] = {'robot:' whoami, 'x': self.mouse.x, 'y': self.mouse.y}
-        elif self.mouse.canGoRight() and self.dx > 0 and (self.dx > self.dy):
-            self.path.append([self.mouse.x, self.mouse.y])
-            self.mouse.goRight()
-			self.neighbors_states[self.whoami] = {'robot:' whoami, 'x': self.mouse.x, 'y': self.mouse.y}
-        elif self.mouse.canGoDown() and self.dy < 0 and (self.dy > self.dx):
-            self.path.append([self.mouse.x, self.mouse.y])
-            self.mouse.goDown()
-			self.neighbors_states[self.whoami] = {'robot:' whoami, 'x': self.mouse.x, 'y': self.mouse.y}
-        else:
-			# define !stop condition
-            if len(self.path) != 0:
-                x, y = self.path.pop()
-                if x < self.mouse.x:
-                    self.mouse.goLeft()s
-                elif x > self.mouse.x:
-                    self.mouse.goRight()
-                elif y < self.mouse.y:
-                    self.mouse.goUp()
-                elif y > self.mouse.y:
-                    self.mouse.goDown()
-            else:
-                self.stop_condition = 1
-
-        sleep(0.5)
+# class StrategyTestRendezvous(Strategy):
+#     mouse = None
+#     # isVisited = []
+#     path = []
+#     isBack = False
+#     network = None
+# 	neighbors_states = {}
+# 	topological_neighbors = []
+# 	num_bots = 0
+# 	stop_condition = False
+# 	whoami = -1
+# 	dx = -1.0
+# 	dy = -1.0
+#
+#     def __init__(self, mouse, num_bots, initPoint):
+# 		# add
+# 		print("hello")
+# 		self.mouse = mouse
+# 		self.num_bots = num_bots
+# 		for i in range(1, num_bots + 1):
+# 			if initPoint[str(i)] != (self.mouse.x, self.mouse.y):
+# 				self.neighbors_states[i] = {'robot:' i, 'x': initPoint[str(i)][0], 'y': initPoint[str(i)][1]}
+# 			else: self.whoami = i
+#         self.network = NetworkInterface()
+#         self.network.initSocket()
+#         self.network.startReceiveThread()
+#
+#     def checkFinished(self):
+# 		threshold = 2.0
+# 		if self.dx <= threshold and self.dy <= threshold:
+# 			self.stop_condition = True
+# 		else:
+# 			self.stop_condition = False
+#         return self.stop_condition
+#
+#     def go(self):
+#         self.mouse.senseWalls()
+#         print(self.mouse.getCurrentCell().getWhichIsWall())
+#         sendData = {'robot': self.whoami , 'x': self.mouse.x, 'y': self.mouse.y, 'up': not self.mouse.canGoUp(
+#         ), 'down': not self.mouse.canGoDown(), 'left': not self.mouse.canGoLeft(), 'right': not self.mouse.canGoRight()}
+#         self.network.sendStringData(sendData)
+#         recvData = self.network.retrieveData()
+#
+# 		# data recieved from neighbors
+# 		while recvData:
+#             some_data = recvData
+#             if some_data['up']:
+#                 self.mouse.mazeMap.setCellUpAsWall(cell)
+#             if some_data['down']:
+#                 self.mouse.mazeMap.setCellDownAsWall(cell)
+#             if some_data['left']:
+#                 self.mouse.mazeMap.setCellLeftAsWall(cell)
+#             if some_data['right']:
+#                 self.mouse.mazeMap.setCellRightAsWall(cell)
+#             recvData = self.network.retrieveData()
+#
+# 		# calculate the direction to pursue
+# 		def slope(self, num_bots):
+# 			# find largest delta x, and delta deltay
+# 			for k in range(1, num_bots + 1):
+# 				if k != self.whoami:
+# 					dx_temp = self.mouse.x - self.neighbors_states[str(k)]['x']
+# 					dy_temp = self.mouse.y - self.neighbors_states[str(k)]['y']
+#
+# 					if dx_temp > dx:
+# 						self.dx = dx_temp # right/left
+# 					if dy_temp > dy:
+# 						self.dy = dy_temp # up/down
+#
+#
+#         if self.mouse.canGoLeft() and self.dx < 0 and (self.dx > self.dy):
+#             self.path.append([self.mouse.x, self.mouse.y])
+# 			self.mouse.goLeft()
+# 			self.neighbors_states[self.whoami] = {'robot:' whoami, 'x': self.mouse.x, 'y': self.mouse.y}
+#         elif self.mouse.canGoUp() and self.dy > 0 and (self.dy > self.dx):
+#             self.path.append([self.mouse.x, self.mouse.y])
+#             self.mouse.goUp()
+# 			self.neighbors_states[self.whoami] = {'robot:' whoami, 'x': self.mouse.x, 'y': self.mouse.y}
+#         elif self.mouse.canGoRight() and self.dx > 0 and (self.dx > self.dy):
+#             self.path.append([self.mouse.x, self.mouse.y])
+#             self.mouse.goRight()
+# 			self.neighbors_states[self.whoami] = {'robot:' whoami, 'x': self.mouse.x, 'y': self.mouse.y}
+#         elif self.mouse.canGoDown() and self.dy < 0 and (self.dy > self.dx):
+#             self.path.append([self.mouse.x, self.mouse.y])
+#             self.mouse.goDown()
+# 			self.neighbors_states[self.whoami] = {'robot:' whoami, 'x': self.mouse.x, 'y': self.mouse.y}
+#         else:
+# 			# define !stop condition
+#             if len(self.path) != 0:
+#                 x, y = self.path.pop()
+#                 if x < self.mouse.x:
+#                     self.mouse.goLeft()s
+#                 elif x > self.mouse.x:
+#                     self.mouse.goRight()
+#                 elif y < self.mouse.y:
+#                     self.mouse.goUp()
+#                 elif y > self.mouse.y:
+#                     self.mouse.goDown()
+#             else:
+#                 self.stop_condition = 1
+#
+#         sleep(0.5)
 
 
 class StrategyTestDFSEV3(Strategy):
