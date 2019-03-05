34114b04aab27920b200d461081e1649b773e897
Allen Spain <aspain2394@gmail.com>
2019-03-05 11:28:29 -0500

    upate

--- framework/strategy.py
+++ framework/strategy.py
@@ -248,16 +248,16 @@ class StrategyTestRendezvous(Strategy):
             if temp_dy > self.dy:
                 self.dy = temp_dx
 
-        if self.mouse.canGoLeft() and self.dx < 0:
+        if self.mouse.canGoLeft() or self.dx < 0:
             self.mouse.goLeft()
             self.neighbors_states[whoami] = {'robot': self.whoami, 'x': self.mouse.x, 'y': self.mouse.y}
-        elif self.mouse.canGoRight() and self.dx > 0:
+        elif self.mouse.canGoRight() or self.dx > 0:
             self.mouse.goRight()
             self.neighbors_states[whoami] = {'robot': self.whoami, 'x': self.mouse.x, 'y': self.mouse.y}
-        elif self.mouse.canGoUp() and self.dy > 0:
+        elif self.mouse.canGoUp() or self.dy > 0:
             self.mouse.goUp()
             self.neighbors_states[whoami] = {'robot': self.whoami, 'x': self.mouse.x, 'y': self.mouse.y}
-        elif self.mouse.canGoDown() and self.dy < 0:
+        elif self.mouse.canGoDown() or self.dy < 0:
             self.mouse.goDown()
             self.neighbors_states[whoami] = {'robot': self.whoami, 'x': self.mouse.x, 'y': self.mouse.y}
         else:
