c4bdc9927556112ee18993cd306cc04cf815f28b
Allen Spain <aspain2394@gmail.com>
2019-03-05 12:42:21 -0500

    upate

--- framework/strategy.py
+++ framework/strategy.py
@@ -243,9 +243,9 @@ class StrategyTestRendezvous(Strategy):
         for k in range(1, num_bots + 1):
             temp_dx = self.neighbors_states[k]['x'] - self.mouse.x
             temp_dy = self.neighbors_states[k]['y'] - self.mouse.y
-            if abs(temp_dx) > abs(self.dx):
+            if temp_dx > self.dx:
                 self.dx = temp_dx
-            if abs(temp_dy) > abs(self.dy):
+            if temp_dy > self.dy:
                 self.dy = temp_dy
 
         if self.mouse.canGoLeft() or self.dx < 0:
