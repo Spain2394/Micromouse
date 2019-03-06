130008baaaf526d097e63a038a8b3f9b1d31b230
Allen Spain <aspain2394@gmail.com>
2019-03-06 16:59:52 -0500

    upate

--- framework/strategy.py
+++ framework/strategy.py
@@ -218,7 +218,7 @@ class StrategyTestRendezvous(Strategy):
     def go(self):
         self.mouse.senseWalls()
         print(self.mouse.getCurrentCell().getWhichIsWall())
-        sendData = {'robot': self.whoami, 'x': self.mouse.x, 'y': self.mouse.y, 'up': not self.mouse.canGoUp(
+        sendData = {'x': self.mouse.x, 'y': self.mouse.y, 'up': not self.mouse.canGoUp(
         ), 'down': not self.mouse.canGoDown(), 'left': not self.mouse.canGoLeft(), 'right': not self.mouse.canGoRight()}
         print(sendData)
 
