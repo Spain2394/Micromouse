91f730a4344a82d9352277aca923531c8d4353c0
Allen Spain <aspain2394@gmail.com>
2019-03-06 17:23:09 -0500

    upate

--- framework/strategy.py
+++ framework/strategy.py
@@ -227,7 +227,7 @@ class StrategyTestRendezvous(Strategy):
         print(recvData)
 
         while recvData:
-            print(recvData)
+            print("got into the loop%s"%srecvData)
             otherMap = recvData
             cell = self.mouse.mazeMap.getCell(otherMap['x'], otherMap['y'])
             self.isVisited[otherMap['x']][otherMap['y']] = 1
