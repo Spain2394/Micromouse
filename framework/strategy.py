e1cd1548f8dc633b9292318384c0f824cb0da837
Allen Spain <aspain2394@gmail.com>
2019-03-06 21:30:09 -0500

    upate

--- framework/strategy.py
+++ framework/strategy.py
@@ -222,7 +222,7 @@ class StrategyTestRendezvous(Strategy):
         print(self.mouse.getCurrentCell().getWhichIsWall())
         sendData = {'x': self.mouse.x, 'y': self.mouse.y, 'up': not self.mouse.canGoUp(
         ), 'down': not self.mouse.canGoDown(), 'left': not self.mouse.canGoLeft(), 'right': not self.mouse.canGoRight()}
-        self.network.sendStringData(sendData)
+        print(self.network.sendStringData(sendData))
         recvData = self.network.retrieveData()
         print("recvData: %s"% recvData)
         while recvData:
