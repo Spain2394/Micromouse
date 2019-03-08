a7c347880b2db08fba5d548114f7e37c182516e2
Allen Spain <aspain2394@gmail.com>
2019-03-08 16:31:11 -0500

    upate

--- framework/strategy.py
+++ framework/strategy.py
@@ -296,6 +296,7 @@ class StrategyTestRendezvous(Strategy):
                 if temp < distance:
                     distance = temp
                     follow_him = bots
+
             return distance,follow_him
 
 
