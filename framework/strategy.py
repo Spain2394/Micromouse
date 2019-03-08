930285e67bbea3c3a9aa6e594263903cd4f4e8e6
Allen Spain <aspain2394@gmail.com>
2019-03-08 16:42:56 -0500

    upate

--- framework/strategy.py
+++ framework/strategy.py
@@ -377,9 +377,10 @@ class StrategyTestRendezvous(Strategy):
 
         print(self.dx)
         print(self.dy)
-        print("priority%s"%priority)
+
         a,b = self.dx, self.dy
         priority = self.check_priority(a, b) # go through top tugs from nearby bots
+        print("priority%s"%priority)
         distance, near_robot = self.distance_to_near_neigh() #
         print("distance to enemy: %s"%distance)
         print("near_robot: %s"%near_robot)
