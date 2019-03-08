6d65a28c2a35168a25bef6f2c420f20c04d720c9
Allen Spain <aspain2394@gmail.com>
2019-03-07 22:44:03 -0500

    upate

--- framework/strategy.py
+++ framework/strategy.py
@@ -345,7 +345,7 @@ class StrategyTestRendezvous(Strategy):
                 self.mouse.goDown()
                 self.neighbors_states[self.whoami] = {'robot': self.whoami, 'x':self.mouse.x , 'y': self.mouse.y}
             else: # if no gradient available, then backtrack
-                if len(self.path) > 15:
+                if len(self.path) != 0:
                     x, y = self.path.pop()
                     if x < self.mouse.x:
                         self.mouse.goLeft()
