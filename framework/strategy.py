eac05732a2d416b5bc2c7c903f48dcde8a62b2a6
Allen Spain <aspain2394@gmail.com>
2019-03-09 00:09:40 -0500

    upate

--- framework/strategy.py
+++ framework/strategy.py
@@ -398,17 +398,15 @@ class StrategyTestRendezvous(Strategy):
                 print('L')
             elif self.neighbors_states[near_robot]['direction'][0] is 'L':
                 priority[0] = 'R'
-                print('L')
+                print('R')
             elif self.neighbors_states[near_robot]['direction'][0] is 'U':
                 priority[0] = 'D'
-                print('L')
+                print('D')
             elif self.neighbors_states[near_robot]['direction'][0] is 'D':
                 priority[0] = 'U'
-                print('L')
+                print('U')
         elif direction < 2:
             priority[0] = self.neighbors_states[near_robot]['direction'][0]
-
-
 
         while not moved and r < len(priority):
             if self.mouse.canGoLeft() and priority[r] is 'L' and not self.isVisited[self.mouse.x-1][self.mouse.y]:
