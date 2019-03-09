3790ada85925405e60dc95aafad133e7e8576a86
Allen Spain <aspain2394@gmail.com>
2019-03-09 14:18:59 -0500

    upate

--- framework/strategy.py
+++ framework/strategy.py
@@ -393,9 +393,7 @@ class StrategyTestRendezvous(Strategy):
             print("distance passed: %s"%distance)
             print("near robot%s"%near_robot)
             # priority_temp = priority[0]
-            print(priority[0])
-            priority[0] = self.neighbors_states[near_robot]['direction'][0]
-            self.isVisited[d]
+            print("priority 1: %s"%priority[0])
         if distance < 1:
             # stop, this will likely form pairs
             self.isBack = True
