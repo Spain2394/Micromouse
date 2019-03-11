aebfab86a36b1140020b1542450962d995dabf47
Allen Spain <aspain2394@gmail.com>
2019-03-10 22:43:35 -0400

    upate

--- framework/strategy.py
+++ framework/strategy.py
@@ -556,7 +556,7 @@ class StrategyTestRendezvous(Strategy):
                 print(self.mouse.mazeMap.getCell(state[0],state[1]).getIsThereWall(d))
                 if self.mouse.mazeMap.getCell(state[0],state[1]).getIsThereWall(d) == False: # while mouse can move in some direction
                     print("got in if ")
-                    delta = direction[d]
+                    delta = direction_list[d]
                     print(delta)
                     next_state = (state[0] + delta[0], state[1] + delta[1])
                     next_cost = cost + 1 if my_dir is d else 2
