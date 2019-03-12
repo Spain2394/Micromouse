8e09cb47b9b226d01e25d94bd45a803762e2dc28
Allen Spain <aspain2394@gmail.com>
2019-03-12 02:20:58 -0400

    upate

--- framework/strategy.py
+++ framework/strategy.py
@@ -626,34 +626,34 @@ class StrategyTestRendezvous(Strategy):
                 i+=1
                 continue #look for another point
 
-            # if self.isVisited[x][y] == 0: # may be some confusion here if not continuos
-                # print("haven't visited:")
-                # print("(x,y:)",(x,y))
+            if self.isVisited[x][y] == 0: # may be some confusion here if not continuos
+                print("haven't visited:")
+                print("(x,y:)",(x,y))
 
-            if self.mouse.x < x and self.mouse.canGoRight():
-                self.path.append([self.mouse.x, self.mouse.y])
-                self.isVisited[self.mouse.x + 1][self.mouse.y] = 1
-                self.mouse.goRight()
-                moved = True
-                print("RIGHT")
-            elif self.mouse.x > x and self.mouse.canGoLeft():
-                self.path.append([self.mouse.x, self.mouse.y])
-                self.isVisited[self.mouse.x-1][self.mouse.y] = 1
-                self.mouse.goLeft()
-                moved = True
-                print("LEFT")
-            elif self.mouse.y > y and self.mouse.canGoUp():
-                self.path.append([self.mouse.x, self.mouse.y])
-                self.isVisited[self.mouse.x][self.mouse.y-1] = 1
-                self.mouse.goUp()
-                moved = True
-                print("UP")
-            elif self.mouse.y < y and self.mouse.canGoDown():
-                self.path.append([self.mouse.x, self.mouse.y])
-                self.isVisited[self.mouse.x][self.mouse.y+1] = 1
-                self.mouse.goDown()
-                moved = True
-                print("DOWN")
+                if self.mouse.x < x and self.mouse.canGoRight():
+                    self.path.append([self.mouse.x, self.mouse.y])
+                    self.isVisited[self.mouse.x + 1][self.mouse.y] = 1
+                    self.mouse.goRight()
+                    moved = True
+                    print("RIGHT")
+                elif self.mouse.x > x and self.mouse.canGoLeft():
+                    self.path.append([self.mouse.x, self.mouse.y])
+                    self.isVisited[self.mouse.x-1][self.mouse.y] = 1
+                    self.mouse.goLeft()
+                    moved = True
+                    print("LEFT")
+                elif self.mouse.y > y and self.mouse.canGoUp():
+                    self.path.append([self.mouse.x, self.mouse.y])
+                    self.isVisited[self.mouse.x][self.mouse.y-1] = 1
+                    self.mouse.goUp()
+                    moved = True
+                    print("UP")
+                elif self.mouse.y < y and self.mouse.canGoDown():
+                    self.path.append([self.mouse.x, self.mouse.y])
+                    self.isVisited[self.mouse.x][self.mouse.y+1] = 1
+                    self.mouse.goDown()
+                    moved = True
+                    print("DOWN")
             # if self.path
             i+=1
         sleep(0.05)
