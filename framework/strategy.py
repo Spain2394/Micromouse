b5c46ea7217b535341aa5de5769a8625cadb798e
Allen Spain <aspain2394@gmail.com>
2019-03-11 23:36:03 -0400

    upate

--- framework/strategy.py
+++ framework/strategy.py
@@ -572,6 +572,7 @@ class StrategyTestRendezvous(Strategy):
         # print("final cost: ", self.cost(goal))
         # print("(x,y)",(self.mouse.x, self.mouse.y))
         open = self.cost(goal)
+        b = list()
         # print("open:", open = self.cost(goal))
         # print(self.cost()) # print me a cost function
         print('--------------------')
