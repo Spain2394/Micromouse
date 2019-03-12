ab055412daaa0be4f1324c1fc1ff4e93dfdb43c9
Allen Spain <aspain2394@gmail.com>
2019-03-12 04:03:31 -0400

    upate

--- framework/strategy.py
+++ framework/strategy.py
@@ -512,7 +512,7 @@ class StrategyTestRendezvous(Strategy):
 
         straight_line = self.distance_to_wall(cell,d)
         gradient = (((x - self.GroupCentroid()[0])**2 + (y-self.GroupCentroid()[1])**2)**(1/2))
-        expense = (beta*gradient + 1/(alpha*straight_line+epsilon))
+        expense = (beta*gradient + 1/(alpha*straight_line+epsilon)) + energy
 
         print("priority: %s"%expense)
         print('----------------------------')
