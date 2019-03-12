78acdd35ff4a0030c3d38553c5d198d7236ddd26
Allen Spain <aspain2394@gmail.com>
2019-03-11 23:34:01 -0400

    upate

--- framework/strategy.py
+++ framework/strategy.py
@@ -580,6 +580,7 @@ class StrategyTestRendezvous(Strategy):
         while len(open)> 0:
             a = open.pop()
             b = list(a[1:])
+            b.sort()
             # b.sort()
             # a.sort()
             # b = list(a[1:])
