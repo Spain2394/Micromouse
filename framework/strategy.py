80537941b533773ab46b4427cbb84e616ca4d0b8
Allen Spain <aspain2394@gmail.com>
2019-03-12 21:43:57 -0400

    upate

--- framework/strategy.py
+++ framework/strategy.py
@@ -570,7 +570,7 @@ class StrategyTestRendezvous(Strategy):
         cell = self.mouse.mazeMap.getCell(self.mouse.x,self.mouse.y)
         direction = self.mouse.direction
         group_centroid = self.GroupCentroid()
-        goal = (8,8)
+        goal = (1,3)
         moves = [0,1,2,3]
 
         print('-----------------------------')
