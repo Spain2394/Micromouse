5ed95b450845e40d9334e9431773961e2928747b
Allen Spain <aspain2394@gmail.com>
2019-03-13 17:07:27 -0400

    upate

--- framework/strategy.py
+++ framework/strategy.py
@@ -601,9 +601,9 @@ class StrategyTestRendezvous(Strategy):
             print("IN")
             # if i == len(action):
             #     break
-            x,y = moves[0]
-            print(moves[0])
-            direction = moves[1]
+            x,y = moves[1]
+            print(moves[1])
+            direction = moves[2]
             print(x)
             print(y)
 
