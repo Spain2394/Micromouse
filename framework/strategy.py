c751e3c0a5bed5518a85dfdfb8d0b02a17bd1a6a
Allen Spain <aspain2394@gmail.com>
2019-03-12 20:23:57 -0400

    upate

--- framework/strategy.py
+++ framework/strategy.py
@@ -603,9 +603,9 @@ class StrategyTestRendezvous(Strategy):
 
                 if self.mouse.x < x and self.mouse.canGoRight():
                     self.isVisited[self.mouse.x + 1][self.mouse.y] = 1
-                     self.mouse.goRight()
-                     moved = True
-                     print("RIGHT")
+                    self.mouse.goRight()
+                    moved = True
+                    print("RIGHT")
                 elif self.mouse.x > x and self.mouse.canGoLeft():
                     self.isVisited[self.mouse.x-1][self.mouse.y] = 1
                     self.mouse.goLeft()
