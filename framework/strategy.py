d226622ebfc3b0457e6fb9a9cb73df66bbdda732
Allen Spain <aspain2394@gmail.com>
2019-03-12 23:43:27 -0400

    upate

--- framework/strategy.py
+++ framework/strategy.py
@@ -466,7 +466,7 @@ class StrategyTestRendezvous(Strategy):
                     if hasBeen[next_state[0]][next_state[1]] == 0: # hasn't been
                         next_cost = cost + 1 if my_dir is d else 2
                         print("state update: %s, in direction: %s"% (next_state,d))
-                        expense = self.priority(next_state, d) 
+                        expense = self.priority(next_state, d) #+cost
                         # print("error")
                         takeAction.append([next_state,d])
                         hasBeen[next_state[0]][next_state[1]] = 1 # you have been here
