e029273d61b6e71efac89c0159d98cf092e1fa1b
Allen Spain <aspain2394@gmail.com>
2019-03-14 02:12:56 -0400

    upate

--- framework/strategy.py
+++ framework/strategy.py
@@ -572,7 +572,7 @@ class StrategyTestRendezvous(Strategy):
         action = self.cost(goal)
 
         print("asdf")
-        for bots in neighbors_states:
+        for bots in self.neighbors_states:
             print("in")
             if (self.neighbors_states[bots]['x'],self.neighbors_states[bots]['y']) != goal:
                 self.isBack = False
