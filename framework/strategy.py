246a9a3b19114118889abbb1091e34909192ee48
Allen Spain <aspain2394@gmail.com>
2019-03-16 02:38:38 -0400

    upate

--- framework/strategy.py
+++ framework/strategy.py
@@ -618,7 +618,7 @@ class StrategyTestRendezvous(Strategy):
                     moved = True
                     self.neighbors_states[self.whoami] = {'robot': self.whoami, 'x':self.mouse.x , 'y': self.mouse.y,'direction':'DOWN'}
                     print("DOWN")
-        sleep(0.1)
+        sleep(0.01)
 
         if moved == False: #backtrack if unable to move by best action
             if len(self.path) != 0:
@@ -645,7 +645,7 @@ class StrategyTestRendezvous(Strategy):
                     print("DOWN")
             else:
                 self.isBack = True
-        sleep(0.1)
+        sleep(0.05)
 
 
     # # def check_priority(self, dx_list, dy_list):
